15efe33fcfb4333358076c9a142936814723ff55
Little-Rookie <768726812@qq.com>
2025-04-13 14:35:33 +0800

    制作登陆注册页面

--- carpooling_platform/src/App.vue
+++ carpooling_platform/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <UserAuth />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserAuth from './views/UserAuth.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserAuth
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
